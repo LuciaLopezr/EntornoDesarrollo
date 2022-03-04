b91e87c6b5e97b05036f7c28c4fc42d268674eab
LuciaLopezr <98594210+LuciaLopezr@users.noreply.github.com>
2022-03-04 14:03:48 +0100

    primer commit

---
+++ EntornoDesarrollo/src/com/test/refactoring/Business/Persona.java
@@ -0,0 +1,19 @@
+package com.test.refactoring.Business;
+/**
+ * @author Lucía
+ * @version 1.1
+ */
+public class Persona {
+	private String tipoCarnet;
+	private String nombre_conductor;
+	private String dniConductor;
+	
+public Persona(String tipoCarnet, String nombre_conductor, String dniConductor){
+		super();
+		this.tipoCarnet = tipoCarnet;
+		this.nombre_conductor = nombre_conductor;
+		this.dniConductor = dniConductor;
+}
+
+	}
+
